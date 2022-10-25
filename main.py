affdf746ec9f295b3558d2ee357d0444507296bb
Sam <finepointdog@gmail.com>
2022-10-24 21:04:25 -0500

    Added semi-working login system

--- main.py
+++ main.py
@@ -6,10 +6,26 @@ import os
 import sys as sus
 import time
 import json
+import pickle as pk 
 
 # Variables
 
 # Preliminary Classes:
+
+    # 'Templates' for accounts
+class account:
+    def __init__(self, username, password, money):
+        self.username = username
+        self.password = password
+        self.money = money
+
+class unit:
+    def __init__(self, health, armor, ammo, skill):
+        self.health = health
+        self.armor = armor
+        self.ammo = ammo
+        self.skill = skill
+
 class items:
     def __init__(self, itemid, name, value, marketable, description):
         self.itemid = itemid
@@ -19,11 +35,6 @@ class items:
         self.description = description
         
     def sellItem(self, modifier):
-        pass
-        
-class units:
-    def __init__(self, unitid):
-        self.unitid = unitid
         pass
     
 class legion(units):
@@ -35,12 +46,28 @@ print("\n86 Python RPG v0.1 \n")
 print("1) Login\n")
 print("2) Create Account\n")
 print("3) Exit\n")
+print("4) Play w/o account\n")
 
 while True:
     answer = int(input("Input: "))
     
     if answer == 1:
+        
+        print("\nEnter your username and password\n")
+        
+        username = input("Username: ")
+        password = input("Password: ")
+        
+        openFile = open(username + ".save", "r")
+        
+        if password == openFile.read(1):
+            
+            print("Successfully logged in.")
+            
+        else: 
+            print("\nInvalid Password, Please Try Again")
         break
+    
     elif answer == 2:
         print("\nEnter a username and password\n")
         username = input("Username: ")
@@ -48,9 +75,9 @@ while True:
         confirmedPassword2 = input("Confirm Password: ")
         if confirmPassword1 == confirmedPassword2:
             password = confirmPassword1 
-            with open(username + ".save", "w") as file:
-                file.writelines(username + "\n")
-                file.writelines(password + "\n")
+            file = open(username + ".save", "w")
+            pk.dump(password, file)
+            file.close
         break
     elif answer == 3:
         os.system("CLS")
@@ -60,19 +87,63 @@ while True:
             if answer.lower() == "yes" or answer.lower() == "exit":
                 sus.exit()
             elif answer.lower() == "no":
+                
                 os.system("CLS")
                 print("86 Python RPG v0.1 \n")
                 print("1) Login\n")
                 print("2) Create Account\n")
                 print("3) Exit\n")
+                print("4) Play w/o account\n")
+                
                 break
+            
             else:
-                print("Invalid Answer, Please Input 'yes' or 'no'\n")       
+                print("Invalid Answer, Please Input 'yes' or 'no'\n")     
+    elif answer == 4:
+        break
     else:
         print("Invalid Answer, Please Input '1' or '2'")
-   
+        
+# Main Game Features/Loop:
+    # Gathering intel
+    # Defending against legion attacks
+    # Attacking the legion
+    # Repairing own units
+    # Expanding base
+    # Resource management
 
-print("Enter your username and password\n")
-username = input("Username: ")
-password = input("Password: ")
-print("game")
+while True:
+    
+    print("\nGameplay\n")
+    print("1) Intel\n")
+    print("3) Military Actions\n")
+    print("3) Units\n")
+    print("4) Homebase\n")
+    
+    answer = input("Action #: ")
+    
+    while True:
+        if answer == 1:
+            
+            print("\nIntel Tab\n")
+            print("1) Recon\n")
+            print("2) Legion\n")
+            print("3) Allies\n")
+            
+            answer = input("Action #: ")
+            
+            if answer == 1:
+                
+            elif answer == 2:
+                
+            elif answer == 3:
+                
+            else:
+                
+        elif answer == 2:
+            
+        elif answer == 3:
+            
+        elif answer == 4:
+            
+        else:
